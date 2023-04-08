43cb396d9e05045203a112b0d411d3c46d27220b
ShrimpScript <ezekielsmits@gmail.com>
2023-04-08 16:45:44 -0700

    d

--- src/Message.tsx
+++ src/Message.tsx
@@ -7,13 +7,10 @@ function Message() {
     >
       <img src="..." className="card-img-top" alt="..." />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+        <h5 className="card-title">Music title</h5>
+        <p className="card-text">Music Description</p>
         <a href="#" className="btn btn-primary">
-          Go somewhere
+          Go to page
         </a>
       </div>
     </div>
